0434fbb887688aeb1df7dc4a6e8f24df0186b7d3
misomarcell <misomarcell@gmail.com>
2016-10-18 13:06:51 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,8 +4,5 @@ arg = "World"
 
 if len(sys.argv) > 1:
     arg = sys.argv[1]
-
-if arg:
-    print("Hello " + str(arg) + "!")
-else:
-    print("Hello World!")
+    
+print("Hello " + str(arg) + "!")
