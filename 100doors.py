a9aa9cfc7c54c9adf121199ee7cb46cd729577ea
misomarcell <misomarcell@gmail.com>
2016-10-24 11:34:09 +0200

    100 Doors

---
+++ 100doors.py
@@ -0,0 +1,19 @@
+﻿doorList = [0 * x for x in range(101)]
+
+doors = "The following doors are open: "
+
+for a in range(1,101):
+    for b in range(1,101):
+        if a % b == 0:
+            if doorList[a] == 0:    #Zárva
+                doorList[a] = 1     #Nyitva
+            else:
+                doorList[a] = 0     #Zárva
+
+for c in range(100):
+    if doorList[c] == 1:
+        doors = doors + str(c) + ", "
+
+doors = doors + "?"
+doors = doors.replace(", ?", "")
+print(doors)
